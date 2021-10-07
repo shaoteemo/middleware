20f455e2619bba73b99d73ee342456d505d12ee0
ShaoTeemo <shaoteemo@qq.com>
2021-10-07 21:26:24 +0800

    1.RabbitMQ文档更新，基础模块构建。

---
+++ rabbitmq/src/main/java/com/shaoteemo/rabbitmq/RabbitMQApplication.java
@@ -0,0 +1,19 @@
+package com.shaoteemo.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 所属项目：middleware
+ * 所属包：com.shaoteemo.rabbitmq
+ *
+ * @author ShaoTeemo
+ * @date 2021/10/7
+ * @since 1.0
+ */
+@SpringBootApplication
+public class RabbitMQApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMQApplication.class , args);
+    }
+}
